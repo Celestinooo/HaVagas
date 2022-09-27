839e6d150188bdbb1404f808f6f0306256e7235e
Celestinooo <001celestino@gmail.com>
2022-09-26 21:09:33 -0300

    Garante salvamento e restauração dos dados do formulário em caso de destruição da tela.

--- app/src/main/java/br/edu/ifsp/scl/pdm/havagas/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/pdm/havagas/MainActivity.java
@@ -1,8 +1,11 @@
 package br.edu.ifsp.scl.pdm.havagas;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Toast;
@@ -10,6 +13,22 @@ import android.widget.Toast;
 import br.edu.ifsp.scl.pdm.havagas.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String nomeCompletoKey = "NOME_COMPLETO_KEY";
+    private static final String emailKey = "EMAIL_KEY";
+    private static final String receberAtualizacoesKey = "RECEBER_ATUALIZACOES_KEY";
+    private static final String tipoTelefoneComercialKey = "TIPO_TELEFONE_KEY";
+    private static final String telefoneKey = "TELEFONE_KEY";
+    private static final String adicionarCelularKey = "ADICIONAR_CELULAR_KEY";
+    private static final String celularKey = "CELULAR_KEY";
+    private static final String sexoFemininoKey = "SEXO_KEY";
+    private static final String formacaoIndexKey = "FORMACAO_KEY";
+    private static final String anoFormaturaKey = "ANO_FORMATURA_KEY";
+    private static final String anoConclusaoKey = "ANO_CONCLUSAO_KEY";
+    private static final String instituicaoKey = "INSTITUICAO_KEY";
+    private static final String monografiaKey = "MONOGRAFIA_KEY";
+    private static final String orientadorKey = "ORIENTADOR_KEY";
+    private static final String vagasDeInteresseKey = "VAGAS_DE_INTERESSE_KEY";
 
     private ActivityMainBinding amb;
     private Formacao formacao = Formacao.ENSINO_FUNDAMENTAL;
@@ -105,5 +124,101 @@ public class MainActivity extends AppCompatActivity {
 
         Candidato candidato = new Candidato(nome, email, receberAtualizacoes, tipoTelefone, telefone, adicionarCelular, celular, sexo, formacao, anoFormatura, anoConclusao, instituicao, monografia, orientador, vagasDeInteresse);
         Toast.makeText(this, candidato.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveFormData(outState);
+        // Para forçar que o formulário seja limpo e checar que os dados dispostos vêm do salvamento,
+        // retire o comentário da linha abaixo
+         limpar();
+    }
+
+    private void saveFormData(@NonNull Bundle bundle){
+        String nome = amb.nomeEt.getText().toString();
+        String email = amb.emailEt.getText().toString();
+        boolean receberAtualizacoes = amb.receberAtualizacoesCb.isChecked();
+        boolean isComercial = amb.comercialRb.isChecked();
+        String telefone = amb.telefoneEt.getText().toString();
+        boolean adicionarCelular = amb.adicionarCelularCb.isChecked();
+        String celular = amb.celularEt.getText().toString();
+        boolean isFeminino = amb.femininoRb.isChecked();
+        String anoFormatura = amb.formaturaEt.getText().toString();
+        String anoConclusao = amb.anoConclusaoEt.getText().toString();
+        String instituicao = amb.instituicaoEt.getText().toString();
+        String monografia = amb.monografiaEt.getText().toString();
+        String orientador = amb.orientadorEt.getText().toString();
+        String vagasDeInteresse = amb.interesseEt.getText().toString();
+
+        bundle.putString(nomeCompletoKey,nome);
+        bundle.putString(emailKey,email);
+        bundle.putBoolean(receberAtualizacoesKey,receberAtualizacoes);
+
+        bundle.putBoolean(tipoTelefoneComercialKey,isComercial);
+        bundle.putString(telefoneKey,telefone);
+        bundle.putBoolean(adicionarCelularKey,adicionarCelular);
+        bundle.putString(celularKey,celular);
+
+        bundle.putBoolean(sexoFemininoKey,isFeminino);
+
+        bundle.putInt(formacaoIndexKey,amb.formacaoSpinner.getSelectedItemPosition());
+
+        bundle.putString(anoFormaturaKey,anoFormatura);
+        bundle.putString(anoConclusaoKey,anoConclusao);
+        bundle.putString(instituicaoKey,instituicao);
+        bundle.putString(monografiaKey,monografia);
+        bundle.putString(orientadorKey,orientador);
+        bundle.putString(vagasDeInteresseKey,vagasDeInteresse);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        restoreFormData(savedInstanceState);
+    }
+
+    private void restoreFormData(@Nullable Bundle bundle){
+        if(bundle == null) return;
+        Log.e("Salvando","Restaurando valores");
+        String nomeCompleto = bundle.getString(nomeCompletoKey);
+        String email = bundle.getString(emailKey);
+        boolean receberAtualizacoes = bundle.getBoolean(receberAtualizacoesKey);
+
+        boolean isComercial = bundle.getBoolean(tipoTelefoneComercialKey);
+        String telefone = bundle.getString(telefoneKey);
+        boolean adicionarCelular = bundle.getBoolean(adicionarCelularKey);
+        String celular = bundle.getString(celularKey);
+
+        boolean isFeminino = bundle.getBoolean(sexoFemininoKey);
+
+        int formacaoIndex = bundle.getInt(formacaoIndexKey);
+
+        String anoFormatura = bundle.getString(anoFormaturaKey);
+        String anoConclusao = bundle.getString(anoConclusaoKey);
+        String instituicao = bundle.getString(instituicaoKey);
+        String monografia = bundle.getString(monografiaKey);
+        String orientador = bundle.getString(orientadorKey);
+        String vagasDeInteresse = bundle.getString(vagasDeInteresseKey);
+
+        amb.nomeEt.setText(nomeCompleto);
+        amb.emailEt.setText(email);
+        amb.receberAtualizacoesCb.setChecked(receberAtualizacoes);
+
+        amb.comercialRb.setChecked(isComercial);
+        amb.telefoneEt.setText(telefone);
+        amb.adicionarCelularCb.setChecked(adicionarCelular);
+        amb.celularEt.setText(celular);
+
+        amb.femininoRb.setChecked(isFeminino);
+
+        amb.formacaoSpinner.setSelection(formacaoIndex);
+        amb.formaturaEt.setText(anoFormatura);
+        amb.anoConclusaoEt.setText(anoConclusao);
+        amb.instituicaoEt.setText(instituicao);
+        amb.monografiaEt.setText(monografia);
+        amb.orientadorEt.setText(orientador);
+        amb.interesseEt.setText(vagasDeInteresse);
     }
 }
